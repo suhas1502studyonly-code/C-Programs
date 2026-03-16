7f36b9213c2f9ae86e87582e2024a43325792845
suhas1502studyonly-code <suhas1502studyonly@gmail.com>
2026-03-16 23:28:14 +0530

    Create p5.c

---
+++ p5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b, c, avg;
+
+    printf("Enter three numbers:\n");
+    scanf("%f %f %f", &a, &b, &c);
+
+    avg = (a + b + c) / 3;
+
+    printf("Average = %.2f\n", avg);
+
+    return 0;
+}
